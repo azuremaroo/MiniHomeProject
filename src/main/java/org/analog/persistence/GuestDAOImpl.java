5f7172677ab1fa5dfc6ce48ae10b4efeac96c539
azuremaroo <azuremaroo@naver.com>
2017-06-30 17:03:01 +0900

    GuestDAOImpl 수정

--- src/main/java/org/analog/persistence/GuestDAOImpl.java
+++ src/main/java/org/analog/persistence/GuestDAOImpl.java
@@ -1,10 +1,24 @@
 package org.analog.persistence;
 
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class GuestDAOImpl implements GuestDAO {
 
+	
+	@Inject
+	private SqlSession sqlSession;
+	
+	private static final String namespace = "org.analog.mappers.guestMappers";
+	
 	@Override
 	public void createGuest(String m_id) {
 		// TODO Auto-generated method stub
+		
+		sqlSession.insert(namespace + ".createGuest", m_id);
 
 	}
 

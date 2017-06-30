443ef69bc722643f29763f34f974a03b0d228e60
azuremaroo <azuremaroo@naver.com>
2017-06-30 17:00:21 +0900

    회원가입시 Guest db 생성

---
+++ src/main/java/org/analog/persistence/GuestDAO.java
@@ -0,0 +1,7 @@
+package org.analog.persistence;
+
+public interface GuestDAO {
+	
+	public void createGuest(String m_id);
+
+}
---
+++ src/main/java/org/analog/persistence/GuestDAOImpl.java
@@ -0,0 +1,11 @@
+package org.analog.persistence;
+
+public class GuestDAOImpl implements GuestDAO {
+
+	@Override
+	public void createGuest(String m_id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

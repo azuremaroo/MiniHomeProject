46fe6b59a82180da1eb4ebeed6b31c36519bdbe6
user <user@175.205.193.27>
2017-07-04 16:49:27 +0900

    2222

--- src/main/java/org/analog/persistence/GuestDAO.java
+++ src/main/java/org/analog/persistence/GuestDAO.java
@@ -1,7 +1,11 @@
 package org.analog.persistence;
 
+import org.analog.domain.GuestVO;
+
 public interface GuestDAO {
 	
 	public void createGuest(String m_id);
+
+	public GuestVO selectGuest(String m_id);
 
 }
--- src/main/java/org/analog/persistence/PBoardManagerDAO.java
+++ src/main/java/org/analog/persistence/PBoardManagerDAO.java
@@ -1,8 +1,13 @@
 package org.analog.persistence;
 
+import java.util.List;
+
+import org.analog.domain.PBoardManagerVO;
 
 public interface PBoardManagerDAO {
 	
 	public void createPBorad(String m_id, String pbm_name);
+
+	public List<PBoardManagerVO> selectPBorad(String m_id);
 
 }
